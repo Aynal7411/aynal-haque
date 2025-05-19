2007c9f032d09777ef4591bcb1f98f762e9d7141
Aynal Haque <aynalhaque7411@gmail.com>
2025-05-19 07:15:24 +0000

    🔐 Removed exposed API key from settings and env

--- aynal_portfolio/settings.py
+++ aynal_portfolio/settings.py
@@ -117,8 +117,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-OPENAI_API_KEY = "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"
 
 
 ROOT_URLCONF = "aynal_portfolio.urls"
