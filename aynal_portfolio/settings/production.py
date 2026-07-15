9295a45c0cff4b7c6f331824fa42f87709de3ce2
Md Aynal Haque <aynalhaque7411@gmail.com>
2026-07-15 16:18:07 +0600

    snippets api developed

--- aynal_portfolio/settings/production.py
+++ aynal_portfolio/settings/production.py
@@ -1,9 +1,24 @@
 import os
 
 import dj_database_url
-
+import sys
 from .base import *
+from dotenv import load_dotenv
 
+load_dotenv()
+
+def main():
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "aynal_portfolio.settings.development",
+    )
+
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
 
 DEBUG = False
 
@@ -12,33 +27,42 @@ DEBUG = False
 # SECURITY
 # ============================================================
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get(
-    "RENDER_EXTERNAL_HOSTNAME"
-)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "aynal-haque.onrender.com",
+]
 
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = [
+    "https://aynal-haque.onrender.com",
+]
 
 if RENDER_EXTERNAL_HOSTNAME:
-    CSRF_TRUSTED_ORIGINS.append(
-        f"https://{RENDER_EXTERNAL_HOSTNAME}"
-    )
+    origin = f"https://{RENDER_EXTERNAL_HOSTNAME}"
+    if origin not in CSRF_TRUSTED_ORIGINS:
+        CSRF_TRUSTED_ORIGINS.append(origin)
 
 
 # ============================================================
 # DATABASE
 # ============================================================
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError(
+        "DATABASE_URL environment variable is not configured."
+    )
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ["DATABASE_URL"],
+    "default": dj_database_url.parse(
+        DATABASE_URL,
         conn_max_age=600,
         conn_health_checks=True,
         ssl_require=True,
@@ -49,16 +73,18 @@ DATABASES = {
 # ============================================================
 # STATIC FILES
 # ============================================================
+
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": (
+            "whitenoise.storage."
+            "CompressedManifestStaticFilesStorage"
+        ),
     },
 }
-
-
 
 
 # ============================================================
@@ -66,9 +92,7 @@ STORAGES = {
 # ============================================================
 
 SECURE_SSL_REDIRECT = True
-
 SESSION_COOKIE_SECURE = True
-
 CSRF_COOKIE_SECURE = True
 
 SECURE_PROXY_SSL_HEADER = (
@@ -77,5 +101,4 @@ SECURE_PROXY_SSL_HEADER = (
 )
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
-
 X_FRAME_OPTIONS = "DENY"
