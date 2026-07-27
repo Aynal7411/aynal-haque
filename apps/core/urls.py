25b1c0e45d87e4ff35b4021e79ad7f7802158878
Md Aynal Haque <aynalhaque7411@gmail.com>
2026-07-27 15:52:28 +0600

    some change

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
    
-    path("", views.homepage_redesign, name="homepage_redesign"), 
+    path("core/", views.homepage_redesign, name="homepage_redesign"), 
     path('skills/', views.skills_view, name='skills'),
     path("legacy/", views.my_page, name="home-page"),
    
