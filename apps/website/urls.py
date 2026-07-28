adf60ecdae8f898247b9f15ca0c3ae2c2e01230b
Md Aynal Haque <aynalhaque7411@gmail.com>
2026-07-28 10:29:43 +0600

    projects and contact remove from website next i want to create contact and projects app

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -1,40 +1,12 @@
 from django.urls import path
-
 from . import views
 
 
 app_name = "website"
 
 urlpatterns = [
-
-    path(
-        "",
-        views.home_view,
-        name="home",
-    ),
-
-    path(
-        "about/",
-        views.about_view,
-        name="about",
-    ),
-
-    path(
-        "expertise/",
-        views.expertise_view,
-        name="expertise",
-    ),
-
-    path(
-        "projects/",
-        views.projects_view,
-        name="projects",
-    ),
-
-    path(
-        "contact/",
-        views.contact_view,
-        name="contact",
-    ),
-
+    path("", views.home_view, name="home"),
+    path("about/", views.about_view, name="about"),
+    path("expertise/", views.expertise_view, name="expertise"),
+   
 ]
--- apps/website/views.py
+++ apps/website/views.py
@@ -1,12 +1,5 @@
 from django.contrib import messages
 from django.shortcuts import render
-from django.views.decorators.http import require_http_methods
-
-from apps.contact.forms import ContactForm
-
-
-
-
 
 def home_view(request):
     return render(request, "website/home.html")
@@ -18,27 +11,4 @@ def about_view(request):
 
 def expertise_view(request):
     return render(request, "website/expertise.html")
-
-
-def projects_view(request):
-    return render(request, "website/projects.html")
-
-
-@require_http_methods(["GET", "POST"])
-def contact_view(request):
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            messages.success(
-                request,
-                "Thanks for reaching out. I will get back to you shortly.",
-            )
-            form = ContactForm()
-        else:
-            messages.error(request, "Please correct the highlighted fields.")
-    else:
-        form = ContactForm()
-
-    return render(request, "website/contact.html", {"form": form})
-
 
